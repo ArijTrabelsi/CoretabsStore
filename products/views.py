9c3df31125be9f189e4e61298143a2ffb78869ec
Arij Trabelsi <arijarouja157@gmail.com>
2020-09-23 00:36:25 +0200

    update

--- products/views.py
+++ products/views.py
@@ -1,12 +1,17 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.utils import timezone
-
+from .models import Product
 
 # Create your views here.
+def products_list(request):
+    products = Product.objects.all()
+    return render(request, 'products-list.html', {'products': products})
 
 def say_hi(request, name):
     return render(request, 'say-hi.html', {'name': name})
     
 def show_time(request):
-    return render(request, 'show-time.html', {'time': timezone.now()})
+    context = {'time': timezone.now()}
+    return render(request, 'show-time.html', context)
+
