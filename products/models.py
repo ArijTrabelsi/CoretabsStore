b6b5e39fc156ad160e092f47e6039f62e2648cb1
Arij Trabelsi <arijarouja157@gmail.com>
2020-09-23 19:53:42 +0200

    Fix

--- products/models.py
+++ products/models.py
@@ -4,7 +4,7 @@ from django.db import models
 
 class Product(models.Model):
     title = models.CharField(max_length=100)
-    description = models.TextField(max_length=225)
+    description = models.TextField(null=True)
 
     def __str__(self):
-        return (self.title, self.description)
+        return self.title
