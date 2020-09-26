0b65516eaee762057887f7390b717d24f2a100b0
Arij <arijarouja157@gmail.com>
2020-09-26 16:42:43 +0200

    Deploying the project

--- store/settings.py
+++ store/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = '-nq9i!wqw5dzf*q%2!4x9ez%#nq4c6)!(d9v=ga#&+#lpb4sds'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = []
 
@@ -122,4 +122,4 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-ALLOWED_HOSTS = ['.herokuapp.com',]
+ALLOWED_HOSTS = ['*']
