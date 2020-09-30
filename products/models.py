92aed64dd814d250a0f7bb7e6919a1a8c6492949
Arij <arijarouja157@gmail.com>
2020-09-30 06:58:37 +0200

    CRUD

--- products/models.py
+++ products/models.py
@@ -4,11 +4,14 @@ from django.urls import reverse
 # Create your models here.
 
 class Product(models.Model):
+    brand = models.CharField(max_length=100)
     title = models.CharField(max_length=100)
     description = models.TextField(max_length=500, blank=True, null=True)
+    price = models.PositiveIntegerField()
 
     def get_absolute_url(self):
         return reverse('product_details', args=(self.id,))
 
     def __str__(self):
         return self.title
+
--- products/views.py
+++ products/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Product
-
+from .forms import AddProductForm
 
 # Create your views here.
 def products_list(request):
@@ -18,3 +18,40 @@ def product_details(request, pk):
     return render(request, 'products/product-details.html', context)
 
 
+def product_add(request):
+    if request.method == 'POST':
+        form = AddProductForm(request.POST)
+
+        if form.is_valid():
+            form.save()
+            return render(request, 'products/product-add-successful.html')
+
+    else:
+        form = AddProductForm()
+    
+    return render(request, 'products/product-add.html', {'form' : form})
+
+def product_edit(request, pk):
+    product = get_object_or_404(Product, pk=pk)
+
+    if request.method == 'POST':
+        form = AddProductForm(request.POST, instance=product)
+
+        if form.is_valid():
+            form.save()
+            return render(request, 'products/product-updated-successful.html')
+
+    else:
+        form = AddProductForm(instance=product)
+    
+    return render(request, 'products/product-edit.html', {'form' : form})
+
+
+def product_delete(request, pk):
+    product = get_object_or_404(Product, pk=pk)
+
+    if request.method == 'POST':
+        product.delete()
+        return render(request, 'products/product-deleted-successful.html')
+    
+    return render(request, 'products/product-delete.html')
