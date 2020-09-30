2dd7c519939605821bc6901b6eba9409402e7586
Arij <arijarouja157@gmail.com>
2020-10-01 00:56:18 +0200

    Store style

--- store/settings.py
+++ store/settings.py
@@ -122,4 +122,12 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static')
+]
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
 ALLOWED_HOSTS = ['.herokuapp.com','127.0.0.1' ]
