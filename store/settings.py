2491ea6b7b985c6f93c793793cc4125bbedb3457
Arij <arijarouja157@gmail.com>
2020-10-05 03:37:29 +0200

    Adding member authentication system

--- store/settings.py
+++ store/settings.py
@@ -102,6 +102,8 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+LOGIN_REDIRECT_URL = 'products_list'
+LOGOUT_REDIRECT_URL = 'products_list'
 
 # Internationalization
 # https://docs.djangoproject.com/en/3.1/topics/i18n/
